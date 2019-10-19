e119b122e98248b5d0f6004b91b84b874f88a26b
khanh1202 <dgkhanh97@gmail.com>
2019-10-19 12:42:00 +1100

    Initialize dummy data

---
+++ src/services/dummyDeveloperService.js
@@ -0,0 +1,9 @@
+export const developers = [
+  { _id: "DP1", name: "Khanh Dinh" },
+  { _id: "DP2", name: "Duong Tran" },
+  { _id: "DP3", name: "Dummy Dev" }
+];
+
+export function getDevelopers() {
+  return priorities.filter(d => d);
+}
