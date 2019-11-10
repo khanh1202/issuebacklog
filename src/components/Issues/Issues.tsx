aa37852f8946055029804e4a5fe50ac8e88a7f54
khanh1202 <dgkhanh97@gmail.com>
2019-11-11 00:30:42 +1100

    feat: Issues table

--- src/components/Issues/Issues.tsx
+++ src/components/Issues/Issues.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { issues, IIssue } from 'services/DummyIssueService';
-import { List, ListItem, ListItemText } from '@material-ui/core';
 import {
   Table,
   TableBody,
@@ -9,7 +8,7 @@ import {
   TableRow
 } from '@material-ui/core';
 
-import { withRouter, WithRouterProps, RouteComponentProps } from 'react-router';
+import { withRouter, RouteComponentProps } from 'react-router';
 
 interface IIssuesState {
   issues: IIssue[];
@@ -36,45 +35,39 @@ export class Issues extends React.Component<RouteComponentProps, IIssuesState> {
     const { issues } = this.state;
 
     return (
-      <List>
-        {issues.map(issue => (
-          <ListItem>
-            <ListItemText
-              primary={issue.name}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Priority</TableCell>
+            <TableCell>Size</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Assigned To</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {issues.map(issue => (
+            <TableRow
+              key={issue._id}
               onClick={this.createIssueClickHandler(issue)}
-            />
-          </ListItem>
-        ))}
-      </List>
-      // <Table>
-      //   <TableHead>
-      //     <TableRow>
-      //       <TableCell>ID</TableCell>
-      //       <TableCell>Name</TableCell>
-      //       <TableCell>Priority</TableCell>
-      //       <TableCell>Size</TableCell>
-      //       <TableCell>Status</TableCell>
-      //       <TableCell>Assigned To</TableCell>
-      //     </TableRow>
-      //   </TableHead>
-      //   <TableBody>
-      //     {issues.map(issue => (
-      //       <TableRow key={issue._id}>
-      //         <TableCell>{issue._id}</TableCell>
-      //         <TableCell>{issue.name}</TableCell>
-      //         <TableCell>{issue.priority.name}</TableCell>
-      //         <TableCell>{issue.size.name}</TableCell>
-      //         <TableCell>{issue.status.name}</TableCell>
-      //         <TableCell>{issue.assignedTo.name}</TableCell>
-      //       </TableRow>
-      //     ))}
-      //   </TableBody>
-      // </Table>
+              hover
+            >
+              <TableCell>{issue._id}</TableCell>
+              <TableCell>{issue.name}</TableCell>
+              <TableCell>{issue.priority.name}</TableCell>
+              <TableCell>{issue.size.name}</TableCell>
+              <TableCell>{issue.status.name}</TableCell>
+              <TableCell>{issue.assignedTo.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     );
   }
 }
 
 //TODO: Research HOC / Higher Order Component
-const WrappedIssues = withRouter(Issues);
+export const WrappedIssues = withRouter(Issues);
 
-export default WrappedIssues;
+// export default WrappedIssues;
